5db0ceb6971744da8d53862ea94644559af4ed7b
George Joseph Kristian <jorji.gjk@gmail.com>
2022-06-07 23:02:13 +0700

    Initial Commit

---
+++ DotaHeroes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DotaHeroes
+//
+//  Created by jorjyeah  on 07/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
